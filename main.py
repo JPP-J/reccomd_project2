0d49ad1d1f91c8cf1041105cb556094b50693dc5
JPP-J <jidapa.cmp63@gmail.com>
2025-01-24 18:20:55 +0700

    Update files for main

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import os
 
 from utils.recomend_extended import *
 
-#
+# for training
 def reccomd_sys(path, metadata_path):
     # Load and prepare data
     print(f'Loading data....\n')
-    item_metadata = pd.read_csv(metadata_path)  # Contains 'item_id' and 'description and eg.,'
+    item_metadata = pd.read_csv(metadata_path)  # Contains 'item_id' and 'description and eg., genre'
     print(item_metadata.tail())
     print(item_metadata.columns)
 
@@ -20,7 +20,7 @@ def reccomd_sys(path, metadata_path):
         user_col='userId',
         item_col='movieId',
         rating_col='rating',
-        frac=0.10,
+        frac=1.00,
         test_size=0.2,
         n_neighbors=10,
         n_recommendations=10
@@ -28,6 +28,7 @@ def reccomd_sys(path, metadata_path):
 
     # Add semantic capabilities
     print(f'\nbuilding recommendation system with semantic query....\n')
+
     # # Generate query embedding using model from SentenceTransformer('all-MiniLM-L6-v2')
     recommender.add_item_embeddings(item_metadata, item_col='movieId')
 
@@ -37,6 +38,49 @@ def reccomd_sys(path, metadata_path):
     print(f"Recommendations for user {random_user} with query '{query}':", recommendations)
 
     return recommender
+
+def usage_saved_model(metadata_path,
+                      path_model: str = 'model/recommender.pkl',
+                      query: str = None,
+                      n_recommendations=10):
+    print(f'Loading saved model....\n')
+
+    with open(path_model, 'rb') as f:
+        recommender = pickle.load(f)
+
+    # Create a dictionary to map movieId to movie title
+    item_metadata = pd.read_csv(metadata_path)
+    movie_id_to_title = dict(zip(item_metadata['movieId'], item_metadata['title']))
+
+    # Select a random user from the available users in the ratings dataset
+    random_user = recommender.get_random_user()
+
+    # Generate recommendations using all methods
+    print(f'\nGernerate recommendations....\n')
+    print(f"Recommendations for user {random_user}")
+
+    rec_cf_user = recommender.recommend_by_user_cf(random_user, n_recommendations)
+    rec_cf_item = recommender.recommend_by_item_cf(random_user, n_recommendations)
+    rec_knn_user = recommender.recommend_by_user_knn(random_user, n_recommendations)
+    rec_knn_item = recommender.recommend_by_item_knn(random_user, n_recommendations)
+    rec_mf = recommender.recommend_by_mf(random_user, n_recommendations)
+
+    print("\nUser-based CF recommendations:", rec_cf_user.index.tolist())
+    print("Item-based CF recommendations:", rec_cf_item.index.tolist())
+    print("User KNN recommendations:", rec_knn_user.index.tolist())
+    print("Item KNN recommendations:", rec_knn_item.index.tolist())
+    print("mf recommendations:", rec_mf.index.tolist())
+
+    # Ensure query is provided
+    if query is None:
+        print("Error: Query parameter is required")
+    else:
+        # Get recommendations for the random user
+        recommendations = recommender.hybrid_recommendation(random_user, query)
+
+        # Convert movie IDs to movie titles
+        recommended_titles = [movie_id_to_title.get(movie_id, "Unknown Movie") for movie_id in recommendations]
+        print(f"\nRecommendations for user {random_user} with query '{query}': \n{recommended_titles}\n")
 
 def save_model(recommender):
     # Saved model
@@ -52,9 +96,17 @@ if __name__=='__main__':
     path = "https://drive.google.com/uc?id=1tRgURjUZ4OSKKhAUSpA5anWs_ZzOQ6Nw"
     metadata_path = "https://drive.google.com/uc?id=15tQrIEUNzLOBeLNeDihK20IbAgzjejay"
 
+    # Training
     recommender = reccomd_sys(path, metadata_path)
 
+    # Saved model to use later
     save_model(recommender)
+
+    # Usage saved model
+    usage_saved_model(metadata_path,
+                      path_model= 'model/recommender.pkl',
+                      query='Comedy',
+                      n_recommendations=10)
 
 
 
